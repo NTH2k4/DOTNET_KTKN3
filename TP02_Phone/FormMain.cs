0142a6da4b301862393c220dae31516786a1b0d5
Beater <trunghieu14092004@gmail.com>
2024-10-23 21:04:38 +0700

    Complete topic 02

---
+++ TP02_Phone/FormMain.cs
@@ -0,0 +1,108 @@
+﻿namespace TP02_Phone
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            using (var context = new PhoneDbContext())
+            {
+                var Phone = new Phone
+                {
+                    Name = txtName.Text,
+                    Price = decimal.Parse(txtPrice.Text),
+                    Quantity = int.Parse(txtQuantity.Text),
+                    Description = txtDescription.Text,
+                };
+                context.Phones.Add(Phone);
+                int check = context.SaveChanges();
+                if (check == 0)
+                {
+                    MessageBox.Show("Thêm mới dữ liệu không thành công!");
+                }
+                else
+                {
+                    MessageBox.Show("Thêm mới dữ liệu thành công!");
+                    dgvPhone.DataSource = context.Phones.ToList();
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (var context = new PhoneDbContext())
+            {
+                var Phone = context.Phones.FirstOrDefault(p => p.Id == int.Parse(txtId.Text));
+                if (Phone != null)
+                {
+                    Phone.Name = txtName.Text;
+                    Phone.Price = decimal.Parse(txtPrice.Text);
+                    Phone.Quantity = int.Parse(txtQuantity.Text);
+                    Phone.Description = txtDescription.Text;
+                    int check = context.SaveChanges();
+                    if (check == 0)
+                    {
+                        MessageBox.Show("Cập nhật dữ liệu không thành công!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cập nhật dữ liệu thành công!");
+                        dgvPhone.DataSource = context.Phones.ToList();
+                    }
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (var context = new PhoneDbContext())
+            {
+                var Phone = context.Phones.FirstOrDefault(p => p.Id == int.Parse(txtId.Text));
+                if (Phone != null)
+                {
+                    context.Remove(Phone);
+                    int check = context.SaveChanges();
+                    if (check == 0)
+                    {
+                        MessageBox.Show("Xóa dữ liệu không thành công!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Xóa dữ liệu thành công!");
+                        dgvPhone.DataSource = context.Phones.ToList();
+                    }
+                }
+            }
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            using (var context = new PhoneDbContext())
+            {
+                var Phone = context.Phones.ToList();
+                dgvPhone.DataSource = Phone;
+            }
+        }
+
+        private void dgvPhone_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtId.Text = dgvPhone.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtName.Text = dgvPhone.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtPrice.Text = dgvPhone.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtQuantity.Text = dgvPhone.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtDescription.Text = dgvPhone.Rows[e.RowIndex].Cells[4].Value.ToString();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (var context = new PhoneDbContext())
+            {
+                dgvPhone.DataSource = context.Phones.Where(p => p.Name.Contains(txtSearch.Text)).ToList();
+            }
+        }
+    }
+}
---
+++ TP02_Phone/PhoneDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TP02_Phone
+{
+    internal class PhoneDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=BEATER\KTKN3; Database=PhoneDb; Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+        public DbSet<Phone> Phones { get; set; }
+    }
+}
